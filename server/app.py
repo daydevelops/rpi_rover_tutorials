0b45c7ef9f260929be22bd60a7a0736d990a12a1
Adam Day <adamday@daydevelops.com>
2019-04-27 17:07:49 -0230

    Set up socketIO and controller input transmission

--- server/app.py
+++ server/app.py
@@ -1,11 +1,40 @@
 from flask import Flask
 from flask import render_template
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
+socketio = SocketIO(app,ping_timeout=3,ping_interval=1)
+
+### Routes ###
 @app.route('/')
 def showController():
     return render_template('controller.html')
 
+
+### SOCKET IO EVENTS ###
+
+@socketio.on('connect')
+def connect():
+    print("client has connected")
+	# power up the robot
+
+@socketio.on('disconnect')
+def disconnect():
+    print('Client disconnected')
+	# power down the robot
+
+@socketio.on('speedInput')
+def speedInput(data):
+    print(data)
+
+@socketio.on('updateTrim')
+def updateTrim(change):
+	print(change)
+
 if __name__ == "__main__":
-	app.run()
+	socketio.run(app)
--- server/static/js/controller.js
+++ server/static/js/controller.js
@@ -1,20 +1,45 @@
+var socket;
 
 function powerOff() {
 	// switch to on button display
 	$('#connected').css('display','none');
 	$('#not-connected').css('display','block');
+
+	closeSocket();
 }
 
 function powerOn() {
 	// switch to controller display
 	$('#not-connected').css('display','none');
 	$('#connected').css('display','block');
+
+	openSocket();
+}
+
+function openSocket() {
+	// initialize socket
+	socket = io.connect('http://' + document.domain + ':' + location.port);
+	// event listener for when the connection to the server is established
+	socket.on('connect', function() {
+		console.log('We are connected!')
+	});
+}
+
+function closeSocket() {
+	// send disconnect msg to server
+	socket.emit('disconnect');
+	socket.disconnect();
+	console.log('We have disconnected');
 }
 
 function updateMotorSpeeds() {
-	console.log('speed: '+$("#speed-input").val()+'    heading: '+$("#heading-input").val())
+	data = {
+		'speed':$('#speed-input').val(),
+		'heading':$('#heading-input').val()
+	}
+	socket.emit('speedInput',data);
 }
 
 function changeTrim(data) {
-	console.log('updating Trim: Left->'+data.L+'    Right->'+data.R);
+	socket.emit('updateTrim',data);
 }
