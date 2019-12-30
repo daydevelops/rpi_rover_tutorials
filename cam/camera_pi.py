664f06f04c4df1371a3acbbda8f4110a2d37be1f
daydevelops <adamday@daydevelops.com>
2019-12-30 15:54:29 +0000

    added new driver for pi-cam

---
+++ cam/camera_pi.py
@@ -0,0 +1,24 @@
+import io
+import time
+import picamera
+from base_camera import BaseCamera
+
+
+class Camera(BaseCamera):
+    @staticmethod
+    def frames():
+        with picamera.PiCamera() as camera:
+            # let camera warm up
+            time.sleep(2)
+            camera.resolution = (220,130)
+            camera.framerate = 30
+            stream = io.BytesIO()
+            for _ in camera.capture_continuous(stream, 'jpeg',
+                                                 use_video_port=True):
+                # return current frame
+                stream.seek(0)
+                yield stream.read()
+
+                # reset stream for next frame
+                stream.seek(0)
+                stream.truncate()
--- server/app.py
+++ server/app.py
@@ -1,9 +1,16 @@
-from flask import Flask
+from flask import Flask, Response
 from flask import render_template
 from flask_socketio import SocketIO, emit
+from importlib import import_module
+import os
 import sys
 import subprocess
 import json
+
+# import camera driver
+sys.path.append('cam')
+from camera_pi import Camera
+
 
 app = Flask(__name__)
 
@@ -21,6 +28,19 @@ socketio = SocketIO(app,ping_timeout=3,ping_interval=1)
 def showController():
 	return render_template('controller2.html')
 
+def gen(camera):
+    """Video streaming generator function."""
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 ### SOCKET IO EVENTS ###
 
